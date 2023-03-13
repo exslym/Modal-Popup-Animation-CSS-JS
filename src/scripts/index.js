576f6480813d0e624f64029a4213b52388617ae7
exslym <76844562+exslym@users.noreply.github.com>
2023-03-13 20:27:03 +0400

    webpack updated, files optimized, project structure updated

---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+import '../styles/index.scss';
+import { modals } from './libs/modals';
+
+if (process.env.NODE_ENV === 'development') {
+	require('../index.html');
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+	'use strict';
+
+	modals('modalBlock', 'openButton', 'closeButton');
+});
+
+
+
